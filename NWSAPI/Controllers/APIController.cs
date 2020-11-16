d75593d87d618e9246e8ef1cf798e4c03b2bc146
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2020-11-16 13:54:53 +0100

    Première révision

---
+++ NWSAPI/Controllers/APIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NWSAPI.Entities;
+using NWSAPI.Filters;
+
+namespace NWSAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class APIController : Controller
+    {
+        public async Task<JsonResult> Index([FromBody]Pagination? pagination)
+        {
+            HttpClient client = new HttpClient();
+            string str = await client.GetStringAsync("https://jsonplaceholder.typicode.com/photos");
+            IEnumerable<Photo> photos = JsonConvert.DeserializeObject<List<Photo>>(str);
+
+            if (pagination != null)
+            {
+                if (pagination.Title != null)
+                    photos = photos.Where(i => i.Title.Contains(pagination.Title));
+
+                int skip = pagination.Elements * (pagination.Page - 1);
+
+                photos = photos.Skip(skip).Take(pagination.Elements);
+            }
+
+            return (Json(photos));
+        }
+    }
+}
---
+++ NWSAPI/Filters/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NWSAPI.Filters
+{
+    public class Pagination
+    {
+        public int Elements { get; set; }
+        public int Page { get; set; }
+        public string Title { get; set; }
+    }
+}
